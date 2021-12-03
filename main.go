742bb391bcd9c8ce41a93894eb8af37df86df3c6
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-03 14:12:24 +0800

    feat(go42-章经): 4-5章

--- main.go
+++ main.go
@@ -3,6 +3,31 @@ package main
 import "fmt"
 
 const s = "GO-study"
+const Pi = 3.1415926
+const b string = "test _  b"
+const (
+	a = iota
+	t = iota
+	c
+)
+
+var Ga int = 99
+
+const v int = 139
+
+func GetGa() func() int {
+	if Ga := 55; Ga > 60 {
+		fmt.Println("GetGa if", Ga)
+	}
+	for Ga := 2; ; {
+		fmt.Println("GetGa for", Ga)
+	}
+	return func() int {
+		Ga += 1
+		return Ga
+	}
+
+}
 
 func main() {
 	var (
@@ -20,4 +45,15 @@ func main() {
 	for i, r := range x {
 		fmt.Printf("%#U, %d \n", r, i)
 	}
+	temp := GetGa()
+
+	fmt.Println("main", temp(), temp(), temp(), temp())
+	v := 1
+	{
+		v := 2
+		fmt.Println(v)
+
+	}
+	fmt.Println(v)
+
 }
