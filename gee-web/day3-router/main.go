d0168153bc731a33f5c7961ff6a941f994056a3a
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-09 22:18:54 +0800

    feat(gee-web): router

---
+++ gee-web/day3-router/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"go-study/gee-web/day3-router/gee"
+	"net/http"
+)
+
+func main() {
+
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
+	r.Run(":9999")
+}
