82294dbc7e5a7b47f53449783fea0ed508623459
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-10 11:22:11 +0800

    feat(oop): polymorphism

--- oop/main.go
+++ oop/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"go-study/oop/encapsulation"
 	"go-study/oop/inheritance"
+	"go-study/oop/polymorphism"
 )
 
 func main() {
 	encapsulation.PrintAuthor()
 	inheritance.GetResponse()
+	polymorphism.NewT()
 }
