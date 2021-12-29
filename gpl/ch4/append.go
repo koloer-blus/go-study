64ebff13f85842fa178c9a7ecb9e42e17d823e4f
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-29 20:17:56 +0800

    feat(gpl): array ~ slice

---
+++ gpl/ch4/append.go
@@ -0,0 +1,18 @@
+package main
+
+func AppendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x)
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
