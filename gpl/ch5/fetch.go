0e684d244d8e1431db0d650ca18a1a514e6ffd8b
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-01-02 14:49:12 +0800

    feat(gpl): func defer

---
+++ gpl/ch5/fetch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	if closeErr := f.Close(); err != nil {
+		err = closeErr
+	}
+	return local, n, err
+}
---
+++ gpl/ch5/outline/main.go
@@ -0,0 +1,15 @@
+package outline
+
+import "golang.org/x/net/html"
+
+func ForEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ForEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
