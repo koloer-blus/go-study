ca244fa70524f1b4be86dbd25a3c0b6994f1946b
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-01-21 09:43:13 +0800

    feat(cmd-todo): cmd_add

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-study
 
 go 1.16
 
-require golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
+require (
+	github.com/gonuts/commander v0.3.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
+)
---
+++ todo/cmd_add.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonuts/commander"
+	"os"
+	"strings"
+)
+
+func makeCmdAdd(filename string) *commander.Command {
+	cmdAdd := func(cmd *commander.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Usage()
+			return nil
+		}
+		w, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			return err
+		}
+		defer w.Close()
+		task := strings.Join(args, " ")
+		_, err = fmt.Fprintln(w, task)
+		fmt.Printf("Task added: %s\n", task)
+		return err
+	}
+	return &commander.Command{
+		Run:       cmdAdd,
+		UsageLine: "add [message]",
+		Short:     "add new todo",
+	}
+}
