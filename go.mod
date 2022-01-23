4ae0b7b19b6309d99c71f27c03028946d8df66b6
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-01-23 16:50:07 +0800

    feat(cmd-todo): list

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gonuts/commander v0.3.1 // indirect
+	github.com/gonuts/flag v0.1.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
---
+++ todo/cmd_list.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gonuts/commander"
+	"github.com/gonuts/flag"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	doneMark1 = "\u2620"
+	doneMark2 = "\u2611"
+)
+
+func makeCmdList(filename string) *commander.Command {
+	cmdList := func(cmd *commander.Command, args []string) error {
+		nflag := cmd.Flag.Lookup("n").Value.Get().(bool)
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		br := bufio.NewReader(f)
+		n := 1
+		for {
+			b, _, err := br.ReadLine()
+			if err != nil {
+				if err != io.EOF {
+					return err
+				}
+				break
+			}
+			line := string(b)
+			if strings.HasPrefix(line, "-") {
+				if !nflag {
+					fmt.Printf("%s %03d: %s\n", doneMark2, n, strings.TrimSpace(line[1:]))
+				}
+			} else {
+				fmt.Printf("%s %03d: %s\n", doneMark1, n, strings.TrimSpace(line))
+			}
+			n++
+		}
+		return nil
+	}
+	flg := *flag.NewFlagSet("list", flag.ExitOnError)
+	flg.Bool("n", false, "only done one")
+	return &commander.Command{
+		Run:       cmdList,
+		UsageLine: "list [options]",
+		Short:     "show list index",
+		Flag:      flg,
+	}
+}
