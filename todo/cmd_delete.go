42b63461d1ca690960f4ab2a2d9ee185a1bc62df
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-01-23 16:26:27 +0800

    feat(cmd-todo): done & delete

---
+++ todo/cmd_delete.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gonuts/commander"
+	"io"
+	"os"
+	"strconv"
+)
+
+func makeCmdDelete(filename string) *commander.Command {
+	cmdDelete := func(cmd *commander.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Usage()
+			return nil
+		}
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				return err
+			}
+			ids = append(ids, id)
+		}
+		w, err := os.Create(filename + "_")
+		if err != nil {
+			return nil
+		}
+		defer w.Close()
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		br := bufio.NewReader(f)
+		for n := 1; ; n++ {
+			b, _, err := br.ReadLine()
+			if err != nil {
+				if err != io.EOF {
+					return nil
+				}
+				break
+			}
+			match := false
+			for _, id := range ids {
+				if id == n {
+					match = true
+				}
+			}
+
+			if match {
+				fmt.Printf("Task Deleted: %s", string(b))
+			} else {
+				_, err = fmt.Fprintf(w, "%s\n", string(b))
+				if err != nil {
+					return err
+				}
+			}
+		}
+		f.Close()
+		w.Close()
+		err = os.Remove(filename)
+
+		if err != nil {
+			return err
+		}
+
+		return os.Rename(filename+"_", filename)
+
+	}
+	return &commander.Command{
+		Run:       cmdDelete,
+		UsageLine: "delete [ID]",
+		Short:     "delete the todo",
+	}
+}
---
+++ todo/cmd_done.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gonuts/commander"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func makeCmdDone(filename string) *commander.Command {
+	cmdDone := func(cmd *commander.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Usage()
+			return nil
+		}
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				return err
+			}
+			ids = append(ids, id)
+		}
+
+		w, err := os.Create(filename + "_")
+		if err != nil {
+			return err
+		}
+		defer w.Close()
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		br := bufio.NewReader(f)
+		for n := 1; ; n++ {
+			b, _, err := br.ReadLine()
+			if err != nil {
+				if err != io.EOF {
+					return err
+				}
+				break
+			}
+			match := false
+			for _, id := range ids {
+				if id == n {
+					match = true
+				}
+			}
+
+			line := strings.TrimSpace(string(b))
+			if match && !strings.HasPrefix(line, "-") {
+				_, err = fmt.Fprintf(w, "-%s\n", line)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("Task done: %s\n", line)
+			} else {
+				_, err = fmt.Fprintf(w, "%s\n", line)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		f.Close()
+		w.Close()
+		err = os.Remove(filename)
+		if err != nil {
+			return err
+		}
+		return os.Rename(filename+"_", filename)
+	}
+	return &commander.Command{
+		Run:       cmdDone,
+		UsageLine: "done [ID]",
+		Short:     "done the todo",
+	}
+}
